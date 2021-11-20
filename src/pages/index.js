60cae6f993e42e1bb27abe7b10f5803a4fb28b8e
Fatih Aktas <akfatih2@gmail.com>
2021-11-20 16:45:48 +0300

    The night version with metaballs

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,11 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { StaticImage } from "gatsby-plugin-image"
+import Helmet from "react-helmet"
+import { withPrefix, Link } from "gatsby"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin, faTwitter, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
-
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -31,76 +32,85 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <Seo title="All posts" />
-      {/* <Bio /> */}
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
-        width={150}
-        height={150}
-        style={{
-          marginBottom: `1.45rem`, 
-          marginLeft: "auto",
-          marginRight: "auto",
-          display: "block",
+      <Helmet>
+        <script src={withPrefix('metaballs.js')} type="text/javascript" />
+      </Helmet>
+      <div class="container">
+        <Seo title="All posts" />
+        {/* <Bio /> */}
+        <div className="flexCenter applyWidth">
+          <img
+            className="bio-avatar"
+            id="profilePicture"
+            layout="fixed"
+            formats={["auto", "webp", "avif"]}
+            src="profile-pic.png"
+            width={200}
+            height={200}
+            quality={95}
+            alt="Profile picture"
+          />
+          <span style={{ textAlign: 'left' }}>
+              Hi there!
+              <br />
+              <br />
+              I'm a Computer Engineer from the University of Alberta.
+              <br />
+              I currently work as a Software Engineer at <a href="https://www.lumnion.com/">Lumnion</a>.
+          </span >
+          
+        </div>
 
-        }}
-        quality={95}
-        alt="Profile picture"
-      />
-      <div style={{textAlign:'center'}}>
-        Computer Engineering Graduate from the University of Alberta.
-        <br />
-        Currently working as a Software Engineer at <a href="https://www.lumnion.com/">Lumnion</a>.
+        <div style={{ display: "flex", justifyContent: 'space-around', flexDirection: 'row', textAlign: 'center', padding: "20px" }}>
+          <a className="content" target="_blank" href="https://github.com/aktasfatih" >
+            <div className="activate" style={{ flex: 1, flexGrow: 1, verticalAlign: 'center', padding: 10, textAlign: 'center' }}>
+              <FontAwesomeIcon icon={faGithub} style={{ display: "inline-block", width: "50px", height: "50px" }} />
+              <div>
+                github.com/aktasfatih
+              </div>
+            </div>
+          </a>
+          <a className="content" target="_blank" href="https://stackoverflow.com/users/5027899/fatih-akta%c5%9f" >
+            <div className="activate" style={{ flex: 1, flexGrow: 1, verticalAlign: 'center', padding: 10, textAlign: 'center' }}>
+              <FontAwesomeIcon icon={faStackOverflow} style={{ display: "inline-block", width: "50px", height: "50px" }} />
+              <div>
+                stackoverflow.com
+              </div>
+            </div>
+          </a>
+        </div>
 
-      </div>
-      <br />
-      <br /> 
-      
-      <div style={{display: "flex", justifyContent:'space-around', flexDirection: 'row', textAlign:'center', padding: "20px"}}>
-        <a className="content" target="_blank" href="https://github.com/aktasfatih" >
-          <div  className="activate" style={{flex:1, flexGrow:1 , verticalAlign:'center', padding: 10, textAlign:'center'}}>
-            <FontAwesomeIcon icon={faGithub} style={{display: "inline-block", width: "50px", height: "50px"}}/>
-            <div>
-            github.com/aktasfatih
+        <div style={{ display: "flex", flexDirection: 'row', justifyContent: 'space-around', textAlign: 'center', padding: "20px" }}>
+          <a className="content" target="_blank" href="https://twitter.com/moreincode" >
+            <div onClick={() => location.href = 'twitter.com/moreincode'} className="activate" style={{ flex: 1, flexGrow: 1, verticalAlign: 'center', padding: 10, textAlign: 'center' }}>
+              <FontAwesomeIcon icon={faTwitter} style={{ display: "inline-block", width: "50px", height: "50px" }} />
+              <div>
+                twitter.com/moreincode
+              </div>
             </div>
-          </div>
-        </a>
-        <a className="content" target="_blank" href="https://stackoverflow.com/users/5027899/fatih-akta%c5%9f" >
-          <div  className="activate" style={{flex:1, flexGrow:1 , verticalAlign:'center', padding: 10, textAlign:'center'}}>
-            <FontAwesomeIcon icon={faStackOverflow} style={{display: "inline-block", width: "50px", height: "50px"}}/>
-            <div>
-              stackoverflow.com
-            </div>
-          </div>
-        </a>
-      </div>
+          </a>
 
-      <div style={{display: "flex", flexDirection: 'row', justifyContent:'space-around', textAlign:'center', padding: "20px"}}>
-        <a className="content" target="_blank" href="https://twitter.com/moreincode" >
-          <div onClick={() => location.href='twitter.com/moreincode'} className="activate" style={{flex:1, flexGrow:1 , verticalAlign:'center', padding: 10, textAlign:'center'}}>
-            <FontAwesomeIcon icon={faTwitter} style={{display: "inline-block", width: "50px", height: "50px"}}/>
-            <div>
-            twitter.com/moreincode
+          <a className="content" target="_blank" href="https://www.linkedin.com/in/fatih-aktas/" >
+            <div onClick={() => { location.href = 'linkedin.com/in/fatih-aktas/' }} className="activate" style={{ flex: 1, flexGrow: 1, verticalAlign: 'center', padding: 10, textAlign: 'center' }}>
+              <FontAwesomeIcon icon={faLinkedin} style={{ display: "inline-block", width: "50px", height: "50px" }} />
+              <div>
+                linkedin.com/in/fatih-aktas/
+              </div>
             </div>
-          </div>
-        </a> 
-
-        <a className="content" target="_blank" href="https://www.linkedin.com/in/fatih-aktas/" >
-          <div onClick={() => {location.href='linkedin.com/in/fatih-aktas/'}} className="activate" style={{flex:1, flexGrow:1 , verticalAlign:'center', padding: 10, textAlign:'center'}}>
-            <FontAwesomeIcon icon={faLinkedin} style={{display: "inline-block", width: "50px", height: "50px"}} />
-            <div>
-              linkedin.com/in/fatih-aktas/
-            </div>
-          </div>
-       </a> 
-      </div>
-      <span style={{fontSize:12}}>
-        <b>Note:</b> Hey, I am currently working on this website. I will be adding more content. In the meantime, you could check out my<a href="http://www.twitter.com/moreincode"> twitter</a> or <a href = "mailto: akfatih2@gmail.com">contact me</a> directly.
-      </span>
-      {/* <ol style={{ listStyle: `none` }}>
+          </a>
+        </div>
+        <div className="flexCenter applyWidth">
+          
+        </div>
+        {/* <div className="flexCenter">
+          <span style={{ textAlign:'left'}}>
+            Here are some things I've written:
+          </span>
+        </div> */}
+        {/* <span style={{ fontSize: 12 }}>
+          <b>Note:</b> Hey, I am currently working on this website. I will be adding more content. In the meantime, you could check out my<a href="http://www.twitter.com/moreincode"> twitter</a> or <a href="mailto: akfatih2@gmail.com">contact me</a> directly.
+        </span> */}
+        {/* <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
 
@@ -132,6 +142,7 @@ const BlogIndex = ({ data, location }) => {
           )
         })}
       </ol> */}
+      </div>
     </Layout>
   )
 }
